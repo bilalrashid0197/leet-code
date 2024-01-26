c030bd469d3e1f6d8b693e88b40f3a628a7448c3
Bilal Rashid <bilalrashid0197@gmail.com>
2024-01-26 15:25:30 -0500

    Updating Two Sum - JS

--- Blind75/twoSum.js
+++ Blind75/twoSum.js
@@ -14,3 +14,26 @@ var twoSum = function(nums, target) {
     }
     
 };
+
+// Solution 2: Dict with key as value and pair as index
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let diffAndIndexDict = {};
+    for(let i = 0; nums.length > i; i++){
+        let difference = target - nums[i];
+        
+        // See if the difference exists in the dict, if it doesn't add key(value)-value(index) pair
+        if(!(difference in diffAndIndexDict)){
+            diffAndIndexDict[nums[i]] = i;
+            
+        // If difference in the dict, then return the two indexes
+        } else if (difference in diffAndIndexDict){
+            return [diffAndIndexDict[difference], i];
+        }
+    }
+    
+};
