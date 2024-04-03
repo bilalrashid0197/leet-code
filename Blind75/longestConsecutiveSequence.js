57fc05c60c52305b21db7b6b3aadd0090cdfbd88
Bilal Rashid <rash8510@mylaurier.ca>
2024-04-03 16:43:19 -0400

    Update the following : Top K Frequent Elements & Longest Consecutive Sequence - JS

--- Blind75/longestConsecutiveSequence.js
+++ Blind75/longestConsecutiveSequence.js
@@ -6,18 +6,23 @@
  * @return {number}
  */
 var longestConsecutive = function(nums) {
+    // Step 1: Create a set of all the unique numbers
     let setOfNumbers = new Set(nums);
     let longest = 0;
+    // Step 2: Loop over each number in the set
     for(let value of setOfNumbers){
+        // Step 3: If the currentValue - 1 is not present in the set we have a start of a new sequence
         if(!(setOfNumbers.has(value - 1))){
-            let lengthOfCurrentSequence = 0;
+            let lengthOfCurrentSequence = 0; // Create a count for the current sequence
+            // Step 4: While the currentValue + 1 exists in the set we have an ongoing sequence
             while(setOfNumbers.has(value + lengthOfCurrentSequence)){
                 lengthOfCurrentSequence += 1;
             };
+            // Step 5: once the sequence eventually runs out we assign "longest" the max value between the previous sequence and the length of the current sequence
             longest = Math.max(longest, lengthOfCurrentSequence);
         };
     };
-    return longest;
+    return longest; 
 };
 
 // Solution 2:
@@ -28,21 +33,24 @@ var longestConsecutive = function(nums) {
  * @return {number}
  */
 var longestConsecutive = function(nums) {
+    // Step 1: Return 0 if the length is zero
     if(nums.length === 0){
         return 0;
     }
+    // Step 2: Sort the numbers in ascending order
     nums.sort((a,b) => a-b);
     let currentLength = 1;
     let longestLength = 1;
+    // Step 3: Loop over the 2nd(1st) element in teh array
     for(let i = 1; nums.length > i; i++){
-        if(nums[i] !== nums[i-1]){
-            if(nums[i] === nums[i-1] + 1){
+        if(nums[i] !== nums[i-1]){ // Step 4: Check if the previous element is equal to the current - still we have a sequence even if the value is there
+            if(nums[i] === nums[i-1] + 1){ // Step 5: If a duplicate doesn't exists check if the current value is equal to the previous+1 - hence saying we have a sequence.
                 currentLength += 1;
             } else {
-                longestLength = Math.max(currentLength, longestLength);
+                longestLength = Math.max(currentLength, longestLength); // Step 6: Once the current value is not equal to the previous value + 1 - we return the max value between current sequence length and longest sequence length;
                 currentLength = 1;
             }
         }
     }
-    return Math.max(longestLength, currentLength);
+    return Math.max(longestLength, currentLength); // Step 7: Since some sequences can last to the end of the array - make sure to return the max value between lonest length and the current length
 };
--- Blind75/topKFrequentElements.js
+++ Blind75/topKFrequentElements.js
@@ -1,21 +1,69 @@
 // Solution 1
+    // Time: O(n log n)
+    // Space: O(n)
 /**
  * @param {number[]} nums
  * @param {number} k
  * @return {number[]}
  */
 var topKFrequent = function(nums, k) {
+    // Step 1: Create an object to count the frequencies of each number
     let freqDict = {};
-    for(let i = 0; nums.length > i; i++){
-        if(!(nums[i] in freqDict)){
+    for (let i = 0; nums.length > i; i++) {
+        if (!(nums[i] in freqDict)) {
             freqDict[nums[i]] = 1;
         } else {
             freqDict[nums[i]] += 1;
         };
     };
+    // Step 2: Convert the frequency object to an array of [number, frequency] pairs, and sort it by frequency in descending order
     let arrayToBeSorted = Object.entries(freqDict)
     arrayToBeSorted.sort(
         (firstElement, secondElement) => secondElement[1] - firstElement[1]
     );
-    return arrayToBeSorted.splice(0,k).map((currentArray) => currentArray[0]);
+    // Step 3: Extract the first 'k' keys from the sorted array of pairs and return them
+    return arrayToBeSorted.splice(0, k).map((currentArray) => currentArray[0]);
 };
+
+// Solution 2:
+    // Time: O(n)
+    // Space: O(n)
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var topKFrequent = function(nums, k) {
+    let finalArray = [];
+    // Step 1: Create an object to count the frequencies of each number
+    let freqDict = {};
+    for (let i = 0; nums.length > i; i++) {
+        if (!(nums[i] in freqDict)) {
+            freqDict[nums[i]] = 1;
+        } else {
+            freqDict[nums[i]] += 1;
+        };
+    };
+    // Step 2: Create an array of length nums + 1 to store all the frequencie.Each index will represent the frequencie while the values will be stored in that frequencie
+    let freqArray = [];
+    for (let i = 0; nums.length >= i; i++) {
+        freqArray.push([]);
+    };
+    // Step 3: Append the frequenices from the object based on each array. We will map each index to the amount of times that number apperaaed (appending the number)
+    for (let [key, freqValue] of Object.entries(freqDict)) {
+        freqArray[freqValue].push(key);
+    };
+    // Step 4: Loop over the array until k is zero and append the top k frequent elements
+    for (let i = freqArray.length - 1; i >= 0; i--) {
+        if (freqArray[i] !== null) {
+            for (let a = 0; a < freqArray[i].length; a++) {
+                finalArray.push(freqArray[i][a]);
+                k--;
+                if (k === 0) {
+                    return finalArray;
+                };
+            };
+        };
+    };
+    return finalArray;
+}
